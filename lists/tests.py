9e377215259bde43af8a436bfb10cc99211089b1
Alex Bostock <alex@bostock.uk>
2022-05-19 12:39:51 +0100

    Add minimal html home page

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,18 @@
+from django.http import HttpRequest
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 from lists.views import home_page
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do</title>', html)
+        self.assertTrue(html.endswith('</html>'))
