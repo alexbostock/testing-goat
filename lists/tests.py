031e970db414b66a03ae0fa674868d6d5ddca124
Alex Bostock <alex@bostock.uk>
2022-05-21 09:12:46 +0100

    Add Item model

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
