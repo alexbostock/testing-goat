a3a8e7cbb55d6aa4f297dfc50810604e9f5b4da5
Alex Bostock <alex@bostock.uk>
2022-05-21 08:53:21 +0100

    Refactor functional test

--- lists/tests.py
+++ lists/tests.py
@@ -13,3 +13,19 @@ class HomePageTest(TestCase):
         response = self.client.post('/', {'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second list item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        self.assertEqual(saved_items[0].text, 'The first list item')
+        self.assertEqual(saved_items[1].text, 'The second list item')
