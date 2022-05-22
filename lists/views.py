d5751b353e064c2a8ed4e80c39c39624d791a4d2
Alex Bostock <alex@bostock.uk>
2022-05-22 07:21:05 +0100

    Change form submission URL

--- lists/views.py
+++ lists/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import redirect, render
 from lists.models import Item
 
 def home_page(request):
-    if(request.method == 'POST'):
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list')
     return render(request, 'home.html')
 
 def view_list(request):
     return render(request, 'list.html', {
         'items': Item.objects.all()
     })
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list/')
