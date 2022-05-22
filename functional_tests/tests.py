d77f6946f11974b546cc9f18cd231b4cfded249b
Alex Bostock <alex@bostock.uk>
2022-05-22 20:03:55 +0100

    Add functional test for page style

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -104,3 +104,27 @@ class NewVisitorTest(LiveServerTestCase):
         self.fail("TODO: complete the test!")
 
         # She visits that URL - her to-do list is still there.
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is centered
+        input = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input.location['x'] + input.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # She starts a new list, and sees that the input box is centered there too
+        input.send_keys('testing')
+        input.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        input = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            input.location['x'] + input.size['width'] / 2,
+            512,
+            delta=10
+        )
