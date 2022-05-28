4d57aa39aebc1e963f3e89833d2619c601a9f6ed
Alex Bostock <alex@bostock.uk>
2022-05-28 14:23:40 +0100

    Add new functional test for item validation

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,7 +1,6 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.common.keys import Keys
 import time
 from unittest import skip
 
@@ -14,15 +13,21 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def wait_for_row_in_list_table(self, row_text):
+    def wait_for_assertion(self, assertion_fn):
         start_time = time.time()
         while True:
             try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
+                return assertion_fn()
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.1)
+
+
+    def assert_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def wait_for_row_in_list_table(self, row_text):
+        self.wait_for_assertion(lambda: self.assert_row_in_list_table(row_text))
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,18 +1,39 @@
+from selenium.webdriver.common.keys import Keys
 from unittest import skip
 from .base import FunctionalTest
 
 class ItemValidationtest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Edith accidentally submits an empty list item.
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # The home page refreshes, and there is an error message.
+        self.wait_for_assertion(
+            lambda: self.assertEqual(
+                self.browser.find_element_by_css_selector('.has-error').text,
+                'List item must not be empty'
+            )
+        )
 
         # She tries again with some text, which now works.
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
 
         # She decides to submit a second blank list item.
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # She receives a similar warning on the list page.
+        self.wait_for_assertion(
+            lambda: self.assertEqual(
+                self.browser.find_element_by_css_selector('.has-error').text,
+                'List item must not be empty'
+            )
+        )
 
         # She can correct it by filling some text in.
-
-        self.fail("To be written")
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
