b0cef88d796366b3e2bf590d21ed89c8c8fb0758
Alex Bostock <alex@bostock.uk>
2022-05-28 15:39:57 +0100

    Report validation errors when creating new lists

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 from lists.models import Item, List
 
@@ -26,3 +27,10 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(saved_items[0].list, list_)
         self.assertEqual(saved_items[1].text, 'The second list item')
         self.assertEqual(saved_items[1].list, list_)
+
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 
 from lists.models import Item, List
@@ -13,7 +14,14 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+    except ValidationError:
+        list_.delete()
+        return render(request, 'home.html', {
+            'error': 'List item must not be empty'
+        })
     return redirect(f'/lists/{list_.id}/')
 
 def add_item(request, list_id):
