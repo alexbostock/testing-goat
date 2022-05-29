423ce2b4a66ceb2799418ed2dad4abcd480b5a86
Alex Bostock <alex@bostock.uk>
2022-05-29 19:39:17 +0100

    Refactor view to use form's save method

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR_MESSAGE
+from lists.models import Item, List
 
 class ItemFormTest(TestCase):
     def test_form_item_input_attributes(self):
@@ -12,3 +13,11 @@ class ItemFormTest(TestCase):
         form = ItemForm({'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR_MESSAGE])
+
+    def test_form_saves_handles_saving_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm({'text': 'My item'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.list, list_)
+        self.assertEqual(new_item.text, 'My item')
