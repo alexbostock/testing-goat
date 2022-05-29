df77dfe5f2e78fad70c267d73d7ca0673abeb57c
Alex Bostock <alex@bostock.uk>
2022-05-29 14:35:40 +0100

    Replace old form input with Django model form

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,10 +1,15 @@
 from django.test import TestCase
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
