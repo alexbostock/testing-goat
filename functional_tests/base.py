5be5b0d4a35024784d7fc775d66bb37a16411585
Alex Bostock <alex@bostock.uk>
2022-06-05 08:56:41 +0100

    Refactor explicit waits in functional tests

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -6,6 +6,18 @@ from unittest import skip
 
 MAX_WAIT = 5
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.2)
+    return modified_fn
+
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
@@ -13,35 +25,27 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def wait_for_assertion(self, assertion_fn):
-        start_time = time.time()
-        while True:
-            try:
-                return assertion_fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.1)
-
     def get_new_item_input(self):
         return self.browser.find_element_by_id('id_text')
 
-    def assert_row_in_list_table(self, row_text):
+    @wait
+    def wait_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def wait_for_row_in_list_table(self, row_text):
-        self.wait_for_assertion(lambda: self.assert_row_in_list_table(row_text))
-
+    @wait
     def wait_to_be_logged_in(self, email):
-        self.wait_for_assertion(
-            lambda: self.browser.find_element_by_link_text('Sign out')
-        )
+        self.browser.find_element_by_link_text('Sign out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email):
-        self.wait_for_assertion(lambda: self.browser.find_element_by_name('email'))
+        self.browser.find_element_by_name('email')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertNotIn(email, navbar.text)
+
+    @wait
+    def wait_for_assertion(self, fn):
+        fn()
